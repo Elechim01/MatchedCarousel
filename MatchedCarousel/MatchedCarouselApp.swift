280eac85939ed3f87d88c6c939a9d6ea794938d0
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-16 22:18:39 +0200

    Initial Commit

---
+++ MatchedCarousel/MatchedCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedCarouselApp.swift
+//  MatchedCarousel
+//
+//  Created by Michele Manniello on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchedCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
